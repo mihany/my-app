b06351ae03bdc3027dbb08d14e8764b8331f09ea
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-06-01 14:08:02 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    basePath: "/my-app",
+    output: "export",
     reactStrictMode: true,
 };
 
